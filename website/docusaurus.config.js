f8aa260058bc27edeef73aed2956e72ba87c0a05
omrishiv <327609+omrishiv@users.noreply.github.com>
2025-05-30 16:28:02 -0700

    update website urls
    Signed-off-by: omrishiv <327609+omrishiv@users.noreply.github.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   // favicon: '',
 
   // Set the production url of your site here
-  url: 'https://agentic-community.com',
+  url: 'https://agentic-community.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
